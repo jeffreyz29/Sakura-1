b73b3eece79545164249e16532d7a8e5fbd7c321
Haitei <vrajakul+0@gmail.com>
2021-10-29 00:30:23 +0200

    refactor: unchecked code task

--- src/tasks/checkUncheckedCodes.ts
+++ src/tasks/checkUncheckedCodes.ts
@@ -2,12 +2,12 @@ import { PRIORITY, TASK_RESULT } from '#constants'
 import { Task } from '#structures'
 import { fetchInvite } from '#utils'
 
-export class InviteTask extends Task {
+export class CheckUncheckedCodesTask extends Task {
 	public async run() {
 		const { invites, queue } = this.container
-		const uncheckedCodes = await invites.readUncheckedCodes()
+		const codes = await invites.read('unchecked')
 
-		for (const { guildId, code } of uncheckedCodes) {
+		for (const { guildId, code } of codes) {
 			const invite = await queue.add(fetchInvite(code), { priority: PRIORITY.CATEGORY })
             const expiresAt = invite?.expiresAt ?? null
             const isPermanent = !Boolean(expiresAt) && !Boolean(invite?.maxAge) && !Boolean(invite?.maxUses)
