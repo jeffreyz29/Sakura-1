3b0d93ef627952ee10b4f67fde44d47f0ec9e676
Haitei <vrajakul+0@gmail.com>
2021-10-29 00:30:36 +0200

    feat: update checked code task

---
+++ src/tasks/updateCheckedCodes.ts
@@ -0,0 +1,21 @@
+import { PRIORITY, TASK_RESULT } from '#constants'
+import { Task } from '#structures'
+import { fetchInvite } from '#utils'
+
+export class CheckUncheckedCodesTask extends Task {
+	public async run() {
+		const { invites, queue } = this.container
+		const codes = await invites.read('checked')
+
+		for (const { guildId, code } of codes) {
+			const invite = await queue.add(fetchInvite(code), { priority: PRIORITY.CATEGORY })
+            const expiresAt = invite?.expiresAt ?? null
+            const isPermanent = !Boolean(expiresAt) && !Boolean(invite?.maxAge) && !Boolean(invite?.maxUses)
+            const isValid = Boolean(invite)
+
+            await invites.upsert(guildId, code, expiresAt, isPermanent, isValid)
+        }
+		
+		return TASK_RESULT.REPEAT
+	}
+}
