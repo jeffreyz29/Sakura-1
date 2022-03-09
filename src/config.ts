fd42feb2c903092aa00f3008415d1d93018300c1
Chiitoi <vrajakul@gmail.com>
2022-03-09 03:28:28 +0100

    refactor: remove unused constants

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,7 @@
 export const ENVIRONMENT = process.env.ENVIRONMENT ?? 'development'
-export const GUILD_ID = BigInt(process.env.GUILD_ID ?? 0)
 export const INVITE_CHECK_COOLDOWN = Number(process.env.INVITE_CHECK_COOLDOWN ?? 0)
 export const REDIS_DB = Number(process.env.REDIS_DB)
 export const REDIS_HOST = process.env.REDIS_HOST
 export const REDIS_PASSWORD = process.env.REDIS_PASSWORD
 export const REDIS_PORT = Number(process.env.REDIS_PORT)
-export const SUPPORT_SERVER_CODE = process.env.SUPPORT_SERVER_CODE
 export const TOKEN = process.env.TOKEN
